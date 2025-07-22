01b218b6be2d388116e605f028a11e95943271a1
PORTATIL_MARCO\marco <marco.casati74@gmail.com>
2025-07-22 13:49:06 +0200

    refactorizacion codigo, para evitar recargar el DOM constantemente

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import "./style.css";
 
 const generarNumeroAleatorio = () => Math.floor(Math.random() * 6) + 1;
 
+/* carga de inicio  y botyonera*/
+
+let imagenDado;
+let puntosElement;
+let mensajeElement;
+
 document.addEventListener("DOMContentLoaded", () => {
+  imagenDado = document.getElementById("imagen-dado");
+  puntosElement = document.getElementById("puntos");
+  mensajeElement = document.getElementById("mensaje-despues-tiros");
   inicio();
   eventos();
 });
@@ -13,8 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const inicio = () => {
   const inicioPuntuacion = 0;
-  document.getElementById("imagen-dado").src = "src/img/cara1.png";
-  document.getElementById("puntos").textContent = inicioPuntuacion;
+  imagenDado.src = "src/img/cara1.png";
+  puntosElement.textContent = inicioPuntuacion;
 };
 
 /* ------------------------------------------------------FUNCION TIRAR DADOS ------------------------------*/
@@ -25,10 +34,7 @@ const tirarDado = () => {
 /* ------------------------------------------------------FUNCION ME PLANTO ----------------------------------*/
 
 const mePlanto = () => {
-  const mensajeElement = document.getElementById("mensaje-despues-tiros");
-  const puntos = document.getElementById("puntos").textContent;
-
-  mensajeElement.textContent = `La partida ha terminado. Tu puntuacion es  ${puntos}`;
+  mensajeElement.textContent = `La partida ha terminado. Tu puntuacion es  ${puntosElement.textContent}`;
 
   document.getElementById("tirar").disabled = true;
   document.getElementById("me-planto").disabled = true;
@@ -38,22 +44,22 @@ const mePlanto = () => {
 const mostarImagenDado = (resultado) => {
   switch (resultado) {
     case 1:
-      document.getElementById("imagen-dado").src = "src/img/cara1.png";
+      imagenDado.src = "src/img/cara1.png";
       break;
     case 2:
-      document.getElementById("imagen-dado").src = "src/img/cara2.png";
+      imagenDado.src = "src/img/cara2.png";
       break;
     case 3:
-      document.getElementById("imagen-dado").src = "src/img/cara3.png";
+      imagenDado.src = "src/img/cara3.png";
       break;
     case 4:
-      document.getElementById("imagen-dado").src = "src/img/cara4.png";
+      imagenDado.src = "src/img/cara4.png";
       break;
     case 5:
-      document.getElementById("imagen-dado").src = "src/img/cara5.png";
+      imagenDado.src = "src/img/cara5.png";
       break;
     case 6:
-      document.getElementById("imagen-dado").src = "src/img/cara6.png";
+      imagenDado.src = "src/img/cara6.png";
       break;
   }
 };
@@ -61,40 +67,32 @@ const mostarImagenDado = (resultado) => {
 /* ------------------------------------------------------FUNCION MOSTRAR MENSAJE ------------------------------*/
 
 const mostraMensaje = (resultado) => {
-  const mensajeElement = document.getElementById("mensaje-despues-tiros");
-
-  if (resultado != 6) {
-    mensajeElement.textContent = `BIEN !!!! ha salido ${resultado} TE ATREVES A SEGUIR ?`;
-  } else {
-    mensajeElement.textContent = `LO SIENTO !!! ha salido ${resultado} GAME - OVER `;
-  }
+  mensajeElement.textContent =
+    resultado != 6
+      ? `BIEN !!!! ha salido ${resultado} TE ATREVES A SEGUIR ?`
+      : `LO SIENTO !!! ha salido ${resultado} GAME - OVER `;
 };
 
 /* -------------------------------------------------FUNCION PARA ACUMULAR PUNTOS-------------------------------*/
 
 const sumarPuntos = (resultado) => {
   if (resultado != 6) {
-    const puntos = document.getElementById("puntos");
-    let puntuacionActual = parseInt(puntos.textContent);
-    let nuevaPuntuacion = puntuacionActual + resultado;
-    puntos.textContent = nuevaPuntuacion;
+    const puntuacionActual = parseInt(puntosElement.textContent);
+
+    const nuevaPuntuacion = puntuacionActual + resultado;
+    puntosElement.textContent = nuevaPuntuacion;
 
     estadoPartida(nuevaPuntuacion);
   } else {
     document.getElementById("tirar").disabled = true;
     document.getElementById("me-planto").disabled = true;
 
-    document.getElementById(
-      "mensaje-despues-tiros"
-    ).textContent = `LO SIENTO !!! ha salido el  ${resultado} GAME - OVER `;
+    mensajeElement.textContent = `LO SIENTO !!! ha salido el  ${resultado} GAME - OVER `;
   }
 };
-
 /* -------------------------------------------------FUNCION CONTROL ESTADO PARTIDA-------------------------------*/
 
 const estadoPartida = (nuevaPuntuacion) => {
-  const mensajeElement = document.getElementById("mensaje-despues-tiros");
-
   if (nuevaPuntuacion > 50) {
     mensajeElement.textContent = `ENHORABUENA!!!!! HAS GANADO LA PARTIDA CON ${nuevaPuntuacion} puntos`;
     document.getElementById("tirar").disabled = true;
@@ -111,8 +109,8 @@ const estadoPartida = (nuevaPuntuacion) => {
 const reiniciarJuego = () => {
   inicio();
 
-  document.getElementById("mensaje-despues-tiros").textContent = "";
-  document.getElementById("imagen-dado").src = "src/img/cara1.png";
+  mensajeElement.textContent = "";
+  imagenDado.src = "src/img/cara1.png";
   document.getElementById("tirar").disabled = false;
   document.getElementById("me-planto").disabled = false;
 };
